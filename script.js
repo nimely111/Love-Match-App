1cbce2de3b19547edb3b2bc1c667751ce15fd74d
nimely111 <samuelehnimely@gmail.com>
2024-03-08 22:10:41 +0000

    continue the snake_case naming convention

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let  person_Name;
 function search(){
      person_Name = prompt('What is your name?');
      person_Age = prompt('What is your age?');
-    var h1Element = document.createElement('h1');
+    var h1_Element = document.createElement('h1');
      love_Match = Math.floor(Math.random() * 100);
-    var text_Answer = document.createTextNode(personName + ', your love match is ' + loveMatch + ' & you are ' + person_Age + ' years old');
-    h1Element.setAttribute('id', 'loveMatch');
-    h1Element.appendChild(text_Answer);
-    document.getElementById('love-found').appendChild(h1Element);
+    var text_Answer = document.createTextNode(person_Name + ', your love match is ' + love_Match + ' & you are ' + person_Age + ' years old');
+    h1_Element.setAttribute('id', 'loveMatch');
+    h1_Element.appendChild(text_Answer);
+    document.getElementById('love-found').appendChild(h1_Element);
     match();
 }
 
