4e02aeec145095ea1e90b2af5e447d4ec148c06b
nimely111 <samuelehnimely@gmail.com>
2024-03-09 00:09:20 +0000

    using document.add event listener function

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ let  person_Name;
 let  love_Match_Result = document.getElementById('loveMatch');
 let  love_Found = document.querySelector('.love-found');
 // search btn
-function search(h1_Element, love_Match){
-        person_Name = prompt('What is your name?');
-        person_Age = prompt('What is your age?');
-        h1_Element = document.createElement('h1');
-        love_Match = Math.floor(Math.random() * 100);
-        h1_Element.setAttribute('id', 'loveMatch');
-        h1_Element.appendChild(document.createTextNode
-            (`${person_Name}, your love match is ${love_Match} & you are ${person_Age} years old`));
-            love_Found.appendChild(h1_Element);
-        match();
-}
+const search = document.addEventListener('submit', function (h1_Element, love_Match){
+    person_Name = prompt('What is your name?');
+    person_Age = prompt('What is your age?');
+    h1_Element = document.createElement('h1');
+    love_Match = Math.floor(Math.random() * 100);
+    h1_Element.setAttribute('id', 'loveMatch');
+    h1_Element.appendChild(document.createTextNode
+        (`${person_Name}, your love match is ${love_Match} & you are ${person_Age} years old`));
+        love_Found.appendChild(h1_Element);
+    match();
+})
+
 
 //match users to their random love match
 function match(p, text_Answer1, result){
